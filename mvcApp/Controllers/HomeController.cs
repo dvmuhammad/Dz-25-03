93a33cbbeacffca4f9bab4a331302e9d273253b6
Muhammad <davlatovmuhammad1264@mail.com>
2021-03-26 23:35:01 +0500

    mvc dapper

---
+++ mvcApp/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using mvcApp.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace mvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            this.config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public IActionResult Index()
+        {
+            var items = GetUsers();
+            return View(items);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<User> GetUsers()
+        {
+            using (var db = Connection)
+            {
+                var result = db.Query<User>("SELECT id, Name, FirstName, Age,  FROM users").ToList();
+                return result;
+            }
+        }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }  
+
+    }
+}
